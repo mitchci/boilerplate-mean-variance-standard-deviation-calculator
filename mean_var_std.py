29e3f5b2f9efa23d1bc98a4187bada4149a77ca1
mitch ci <mitchellci@outlook.com>
2024-08-29 08:54:58 +0000

    Chore: init

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    calculations = {
+        "mean": [list(np.mean(b, axis=0)), list(np.mean(b, axis=1)), np.mean(b)],
+        "variance": [list(np.var(b, axis=0)), list(np.var(b, axis=1)), np.var(b)],
+        "standard deviation": [list(np.std(b, axis=0)), list(np.std(b, axis=1)), np.std(b)],
+        "max": [list(np.max(b, axis=0)), list(np.max(b, axis=1)), np.max(b)],
+        "min": [list(np.min(b, axis=0)), list(np.min(b, axis=1)), np.min(b)],
+        "sum": [list(np.sum(b, axis=0)), list(np.sum(b, axis=1)), np.sum(b)],
+    }
     return calculations
